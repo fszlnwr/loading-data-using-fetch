e2b40c4a3bd13557f6d551926256842bdaca9c93
Faizal A <fszlnwr@gmail.com>
2021-03-11 20:59:40 +0700

    add d3 csv

--- src/index.js
+++ src/index.js
@@ -2,7 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as d3 from 'd3'
+
+
+// const fetchText = async(url) => {
+//   const response = await fetch(url);
+//   return await response.text();
+// };
+const csvUrl = 'https://gist.githubusercontent.com/fszlnwr/b54bbfadddded31c3445d1ddda347046/raw/css_named_colors.csv';
+d3.csv(csvUrl).then(data => {
+  let message = '';
+  message = message + Math.round(d3.csvFormat(data).length / 1024) + ' kB \n';
+  message = message + data.length + ' rows \n';
+  message = message + data.columns.length + ' columns \n';
+  document.getElementById('message-container').textContent = message;
+});
+
+// fetchText(csvUrl).then(text => {
+//   const data = d3.csvParse(text);
+//   let message = '';
+//   message = message + Math.round(text.length / 1024) + ' kB \n';
+//   message = message + data.length + ' rows \n';
+//   message = message + data.columns.length + ' columns \n';
+//   document.getElementById('message-container').textContent = message;
+// });
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +35,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
