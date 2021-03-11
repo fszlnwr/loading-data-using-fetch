d5adcf300241304168fdbd5452502042f5ed69a2
Faizal A <fszlnwr@gmail.com>
2021-03-11 21:57:08 +0700

    load d3 csv react state

---
+++ src/Message.js
@@ -0,0 +1,8 @@
+import * as d3 from 'd3';
+export const message = data => {
+    let message = '';
+    message = message + Math.round(d3.csvFormat(data).length / 1024) + ' kB \n';
+    message = message + data.length + ' rows \n';
+    message = message + data.columns.length + ' columns \n';
+  return message;
+};
--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as d3 from 'd3'
+import * as d3 from 'd3';
+import { message } from './Message'
 
 
 // const fetchText = async(url) => {
 //   const response = await fetch(url);
 //   return await response.text();
 // };
-const csvUrl = 'https://gist.githubusercontent.com/fszlnwr/b54bbfadddded31c3445d1ddda347046/raw/css_named_colors.csv';
-d3.csv(csvUrl).then(data => {
-  let message = '';
-  message = message + Math.round(d3.csvFormat(data).length / 1024) + ' kB \n';
-  message = message + data.length + ' rows \n';
-  message = message + data.columns.length + ' columns \n';
-  document.getElementById('message-container').textContent = message;
-});
-
 // fetchText(csvUrl).then(text => {
 //   const data = d3.csvParse(text);
 //   let message = '';
@@ -26,6 +17,21 @@ d3.csv(csvUrl).then(data => {
 //   message = message + data.columns.length + ' columns \n';
 //   document.getElementById('message-container').textContent = message;
 // });
+
+const csvUrl = 'https://gist.githubusercontent.com/fszlnwr/b54bbfadddded31c3445d1ddda347046/raw/css_named_colors.csv';
+
+
+
+const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    d3.csv(csvUrl).then(setData);
+  }, []);
+
+  return <pre>{data ? message(data) : 'loading'}</pre>
+};
+
 
 
 ReactDOM.render(
